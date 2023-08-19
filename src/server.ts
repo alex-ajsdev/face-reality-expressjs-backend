9ba1954e5c8bbb6721d13f4b3c891c49629119ae
Alexander Suarez <alexander.suarez@students.plymouth.ac.uk>
2023-08-19 13:04:40 +0100

    moved mongoose stuff from app.ts to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import app from './app';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+if (!MONGO_URI) {
+  console.error('MONGO_URI not provided. Terminating application.');
+  process.exit(1);
+}
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Could not connect to MongoDB Atlas:', err));
